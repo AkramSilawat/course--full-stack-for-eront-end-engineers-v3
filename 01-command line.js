3de86988b64c90db0620c787cad26e6cdabf9f43
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-03-12 20:13:58 +0530

    Command Line

---
+++ 01-command line.js
@@ -0,0 +1,23 @@
+// ==========> Command Line <========== //
+
+// ===> Diving in
+// ---> Terminal and Command line
+// ---> Vim
+// ---> Shells
+
+
+// --> Terminal and Command line
+
+/*
+    - ls = list directory contents
+    - cd = change directory 
+    - mkdir = Make directory 
+    - rmdir = Remove directory 
+         
+     - cat = Show file contents
+     - man = command manual
+     - less = Show file content by page
+     - touch = creates an Empty File
+     - rm = remove File
+     - echo = repeat input
+    */
