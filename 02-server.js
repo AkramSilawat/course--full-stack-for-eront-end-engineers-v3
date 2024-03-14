75a2f2b00d65d64e032ff2dd2e9915653d17d69a
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-03-14 23:27:53 +0530

    Server

---
+++ 02-server.js
@@ -0,0 +1,34 @@
+// ==========>> Server <<============ //
+
+/*
+    *  What is a server
+    *  Cloud Computing
+    *  VPS
+    *  Operating System
+    *  SSH
+*/        
+
+
+// ===>> What is a server 
+
+// --->> Basic node server 
+
+// 1. Make a file called simpleServer.js
+// 2. Creating a simple HTTP server using Node.js running on port 3000
+
+
+
+
+// --->> Basic node server 
+
+// const http = require("http");
+// const fs = require('fs');       // ---> fs - file system
+// const PORT = 3000;
+
+// const server = http.createServer(function (req, res) {
+//     res.writeHead(200, {'content-type': 'text/html'});
+//     fs.createReadStream('index.html').pipe(res);
+// });
+
+// server.listen(PORT);
+// console.log(`Server started on port ${PORT}`);
