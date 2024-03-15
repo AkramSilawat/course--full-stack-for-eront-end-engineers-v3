98a50147fc903153535560a20bf1bbe9835865ba
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-03-15 18:22:23 +0530

    Server

--- 02-server.js
+++ 02-server.js
@@ -32,3 +32,46 @@
 
 // server.listen(PORT);
 // console.log(`Server started on port ${PORT}`);
+
+
+
+
+
+
+// ==========> Create a simple Node.js Server <========== //
+
+// ->  ~ cd temp
+
+// ->  temp  vi simpleServer.js
+
+
+// ->  temp  vi index.html
+
+// ->  temp  node --version
+
+// ->  temp  brew install node
+
+// ->  temp  nvm use 19
+// -->> Now using node v19.7.0(npm v9.5.9)
+
+// ->  temp node simpleServer.js
+// -->> server started on port 3000
+
+
+
+
+
+// ===========> Server Management <=========== //
+
+
+// --->> Cloud Computing
+
+/*
+    Virtulization : Dividing a Server resourses into virtual computer
+    Virtual Machine : Digital Version of a  physical computer
+    VPS : Virtual Private Server.
+    */
+   
+        
+// ===========> Buying a VPS <============ //
+
