ccb5e6b718294c07755ffa2854b61f30ca97d547
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-03-15 23:08:26 +0530

    Operating System

---
+++ 03-operating systems.js
@@ -0,0 +1,27 @@
+// ============> Operating System <============ //
+
+// --->> Operating System 
+/*
+      1. User
+      2. Shell 
+      3. Kernal_______
+|____ 4. Hardware_____|
+    */            
+
+
+// ============> Security & Hashing <============ //
+
+// --->> Security 
+        
+/*
+    * Username / Password
+    * Biomatric
+    * SSH Key
+    */
+
+
+// --->> What is Hashing?
+// --> Hashing algorithms are One-ways while encryption algorithms are two-ways..
+
+
+// --->> openssl(OPENSSL):- openssl is a cryptography toolkit implementing the TLS(Transport Layer Security)  
