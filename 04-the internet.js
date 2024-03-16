3aa6df840e20d0c6f7850c7f9d4592bd1fb2a237
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-03-16 19:32:48 +0530

    the internet

---
+++ 04-the internet.js
@@ -0,0 +1,38 @@
+// ============> How the internet Works <============ //
+/*
+// -> Understanding the internet
+
+    * How the internet works
+    * Buying a domain
+    * putting it together
+*/
+
+// -------------------------------------
+
+// ==>> How does the internet work?
+
+//            magic?
+// Fornt  ---------------->>
+
+//           Extra magic?
+//        <<----------------  Backand
+
+// --------------------------------------
+
+// ==>> The internet is built on cooperation and rules
+
+    /*
+        computer
+             |
+            \|/
+        network card                          Server
+             |                                  /|\
+            \|/                                  |          
+           router                           Load balancer
+             |                                  /|\
+            \|/                                  | 
+            ISP                              Server cluster
+             |                                  /|\
+            \|/                                  |  
+        Tier 1 ISP  --->  Tier 1 ISP  ----->  Datacenter
+    */
