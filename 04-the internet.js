5777b49e4d7fa2a3e80828b644d0a27b6766187a
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-03-17 13:14:46 +0530

    the internet

--- 04-the internet.js
+++ 04-the internet.js
@@ -36,3 +36,41 @@
             \|/                                  |  
         Tier 1 ISP  --->  Tier 1 ISP  ----->  Datacenter
     */
+
+
+
+// -->> Terminology
+    /*
+        Internet -  A Network of Networks
+
+        Intranet -  Private Network
+
+        LAN -  Local Area Network
+
+        WAN -  Wide area Network
+
+        */
+
+     /*
+        IP - Internet Protocl
+
+        IP Address - numerical label assigned to  an internet connected device.
+
+        IPv34                  IPv6
+        0.0.0.0                2001:db8::ff00:4288   
+        */   
+
+
+
+// -->> Terminology
+    /* 
+        IP -  internet protocol
+
+        IP Address -  numeric label assigned to an internet connected device
+
+    IPv4                   IPv6
+    8.8.8.8          2001:4860:4560:8888   
+        */
+
+
+
