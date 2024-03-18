ae5cc78ff39c4d9aa44e2bcea85af7fa30ee9a49
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-03-18 22:48:48 +0530

    Server.js

--- 02-server.js
+++ 02-server.js
@@ -11,6 +11,12 @@
 
 // ===>> What is a server 
 
+/* - A server is a computer or system that provides resources, data, services, 
+     or programs to other computers, known as clients, over a network. In theory, 
+     whenever computers share resources with client machines they are considered servers.     
+     */
+
+
 // --->> Basic node server 
 
 // 1. Make a file called simpleServer.js
@@ -77,3 +83,4 @@
         
 // ===========> Buying a VPS <============ //
 
+// --> www.DigitalOcean.com
